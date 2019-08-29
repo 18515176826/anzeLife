435457a5ed5d30e33a71f95b2e5205e7232d5121
zhangbo <1057587665@qq.com>
2019-08-29 23:37:09 +0800

    富文本编辑器

---
+++ anzeLife/src/anze/childCom/edition-contains.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="issueTit">内容填写</div>
+    <vue-html5-editor :content="content"  :height="500" :z-index="1000" :auto-height="true" :show-module-name="false"></vue-html5-editor>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      content:''
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.issueTit {
+  height: 2.5rem;
+  font-size: 16px;
+  background: -webkit-linear-gradient(left, #207fff , #6bbfff); /* Safari 5.1 - 6.0 */
+  background: -o-linear-gradient(left, #207fff , #6bbfff); /* Opera 11.1 - 12.0 */
+  background: -moz-linear-gradient(left, #207fff , #6bbfff); /* Firefox 3.6 - 15 */
+  background: linear-gradient(to left, #207fff , #6bbfff); /* 标准的语法 */
+  color: #fff;
+  line-height: 2.5rem;
+  text-align: center;
+}
+</style>
---
+++ anzeLife/src/anze/childCom/regional-choice.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <van-popup
+      v-model="show"
+      position="bottom"
+    >
+    <van-area :area-list="areaList" :columns-num="3" title="标题" />
+    </van-popup>
+  </div>
+</template>
+<script>
+import placeJson from '@/lib/js/place.js'
+export default {
+  data() {
+    return {
+      areaList: placeJson
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
